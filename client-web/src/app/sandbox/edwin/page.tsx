9efd642d566e9998bf22e3634b8349917f92e276
Edwin Lim <edwin_lim@tech.gov.sg>
2025-02-27 18:39:04 +0800

    add dayjs, wip timetable

--- client-web/src/app/sandbox/edwin/page.tsx
+++ client-web/src/app/sandbox/edwin/page.tsx
@@ -1,3 +1,17 @@
+import TimeTable from "@/components/meeting/timetable";
+import dayjs from "dayjs";
+
+const TEST_DATERANGE = {
+  start: dayjs(new Date(2025, 1, 1)),
+  end: dayjs(new Date(2025, 1, 3)),
+};
+
+const TEST_TIMERANGE = { start: 0, end: 24 };
+
 export default async function Sandbox() {
-  return <></>
+  return (
+    <div>
+      <TimeTable dateRange={TEST_DATERANGE} timeRange={TEST_TIMERANGE} />
+    </div>
+  );
 }

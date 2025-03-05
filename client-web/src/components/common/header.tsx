fc842ca7c293c02aa80e1fc6dbdb65461f204604
Joel Tan <Joel_TAN@tech.gov.sg>
2025-03-05 10:16:26 +0800

    update header to be sticky and increase padding

--- client-web/src/components/common/header.tsx
+++ client-web/src/components/common/header.tsx
@@ -3,8 +3,8 @@ import { CalendarCheck2 } from "lucide-react";
 
 export default function Header() {
 	return (
-		<Card className="w-full bg-indigo-600 text-white rounded-none shadow-md">
-			<CardHeader className="flex justify-between items-center p-4">
+		<Card className="w-full bg-indigo-600 text-white rounded-none shadow-md sticky top-0">
+			<CardHeader className="flex justify-between items-center p-6">
 				<span className="text-lg font-bold">
 					schedulr <CalendarCheck2 className="w-4 h-4 inline-block ml-1" />
 				</span>

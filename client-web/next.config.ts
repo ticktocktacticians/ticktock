598e03ed5e4843b20f22ad685be6a02d881b722c
ticktocktacticians <ticktocktacticians@gmail.com>
2025-03-07 10:18:09 +0800

    ignore eslint during prod build

--- client-web/next.config.ts
+++ client-web/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    compiler: {
-        reactRemoveProperties: { properties: ["^data-testid$"] },
-    },
+	compiler: {
+		reactRemoveProperties: { properties: ["^data-testid$"] },
+	},
+	eslint: {
+		// Disable ESLint during production builds
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
